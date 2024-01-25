bd5a3ac44c1cb3adb8a0ffe0a9e511e21ce4bb2d
akhilaiswarya <aishwarya1403ca@gmail.com>
2024-01-25 21:16:10 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -5,7 +5,8 @@ from app.forms import *
 from django.http import HttpResponse,HttpResponseRedirect
 from django.urls import reverse
 from django.core.mail import send_mail
-from django.contrib.auth import authenticate,login
+from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth.decorators import login_required
 def registration(request):
     ufo=UserForm()
     pfo=ProfileForm()
@@ -61,3 +62,16 @@ def user_login(request):
             return HttpResponse('invald credentials')
 
     return render(request,'user_login.html')
+
+@login_required
+def user_logout(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('home'))
+
+
+def profile_display(request):
+    un=request.session.get('username')
+    UO=User.objects.get(username=un)
+    PO=Profile.objects.get(username=UO)
+    d={'UO':UO,'PO':PO}
+    return render(request,'profile_display.html',d)
