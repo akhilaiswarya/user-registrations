d8deec4b2233ad4dd95e3a2a41a7c55c6a5bce36
akhilaiswarya <aishwarya1403ca@gmail.com>
2024-01-26 21:18:58 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -75,3 +75,18 @@ def profile_display(request):
     PO=Profile.objects.get(username=UO)
     d={'UO':UO,'PO':PO}
     return render(request,'profile_display.html',d)
+
+
+
+
+@login_required
+def change_password(request):
+    if request.method=='POST':
+        pw=request.POST['pw']
+        username=request.session.get('username')
+        UO=User.objects.get(username=username)
+        UO.set_password(pw)
+        UO.save()
+        return HttpResponse('Password has changed successfully')
+    
+    return render(request,'change_password.html')
